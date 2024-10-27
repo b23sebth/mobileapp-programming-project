a0b95d5f2cc3574d482b4dd7efabc619114fad4a
b23sebth <b23sebth@student.his.se>
2024-10-27 21:05:13 +0100

    Added second activity for about page and buttons to switch between home/about.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.project;
 
+
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -11,8 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Button button = findViewById(R.id.button_mainactivity);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, com.example.project.SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-
 }
---
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -0,0 +1,28 @@
+package com.example.project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Button button = findViewById(R.id.button_secondactivity);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        TextView textView = findViewById(R.id.textview2);
+        textView.setText(getString(R.string.text_secondactivity));
+
+    }
+}
