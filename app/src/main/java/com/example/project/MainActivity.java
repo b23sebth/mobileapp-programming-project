a5aa994638a5efaa2cb8128d9b0439460c219d24
b23sebth <b23sebth@student.his.se>
2024-10-28 00:30:19 +0100

    Imported and initialized recyclerview.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,23 +1,54 @@
 package com.example.project;
 
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<Music> artists = new ArrayList<>();
+    Gson gson = new Gson();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String json = loadJSONFromAsset("music.json");
+        Log.d("MainActivity", json);
+
+        Type type = new TypeToken<ArrayList<Music>>() {
+        }.getType();
+        ArrayList<Music> artists = gson.fromJson(json, type);
+
+        RecyclerView recycler_view = findViewById(R.id.recycler_view);
+
+        music_RecyclerViewAdapter adapter = new music_RecyclerViewAdapter(this, artists);
+        recycler_view.setAdapter(adapter);
+        recycler_view.setLayoutManager(new LinearLayoutManager(this));
+        adapter.notifyDataSetChanged();
+
         Button button = findViewById(R.id.button_mainactivity);
-        button.setOnClickListener(new View.OnClickListener(){
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, com.example.project.SecondActivity.class);
@@ -25,4 +56,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private String loadJSONFromAsset(String fileName) {
+        String json = null;
+        try {
+            InputStream inputStream = getAssets().open(fileName);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            json = stringBuilder.toString();
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
 }
+
